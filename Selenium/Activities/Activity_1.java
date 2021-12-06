6af87cf491cca4c3a3f4a30d8e03f0c05154a691
vaibhavigithub <61791497+vaibhavigithub@users.noreply.github.com>
2021-12-06 17:15:02 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity_1.java
@@ -0,0 +1,20 @@
+package activites;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity_1 {
+
+	public static void main(String[] args) {
+		 
+		        WebDriver driver = new FirefoxDriver();
+				
+	
+		        driver.get("https://www.training-support.net");
+				
+		        
+		        driver.close();
+		   
+	}
+
+}
